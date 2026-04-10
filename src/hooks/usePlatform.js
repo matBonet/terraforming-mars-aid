05944776889aaa9d9e89061dc23c6939a950be6b
Mateus Bonet <mateus.bonet@gmail.com>
2026-04-10 17:24:23 -0300

    Updates

--- src/hooks/usePlatform.js
+++
@@ -1,8 +0,0 @@
-import { useMediaQuery, useWindowSize } from "@uidotdev/usehooks";
-
-export function usePlatform() {
-  const isMobile = useMediaQuery("(hover: none) and (pointer: coarse)");
-  const { width, height } = useWindowSize();
-  const isHorizontal = width >= 1.3 * height;
-  return { isMobile, isHorizontal };
-}
